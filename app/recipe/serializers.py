720f4088eba29f3627710cf91cceae67dfcd879e
philipkogel <philipkogel@yahoo.pl>
2022-08-15 14:17:28 +0200

    Implemented ingredients delete and create recipe with new and existing ingredients with tests.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -55,11 +55,23 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed."""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -57,6 +57,7 @@ class TagViewSet(
 
 class IngredientViewSet(
     mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet
 ):
