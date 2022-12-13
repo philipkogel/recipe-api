8ba2d7280826e159ba9f93f333e57bb5bd1e2e6c
philipkogel <philipkogel@yahoo.pl>
2022-12-13 14:57:28 +0100

    Django settings update, fix run.sh typo

--- Dockerfile
+++ Dockerfile
@@ -38,4 +38,4 @@ ENV PATH="/scripts:/py/bin:$PATH"
 
 USER django-user
 
-CMD [ "run.sh" ]
+CMD ["run.sh"]
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,4 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-uswgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
